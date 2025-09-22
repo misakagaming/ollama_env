8ea597209883819ae13a69d3c54c8d2dff0c13e2
Eray Erer <eray.erer@hotmail.com>
2025-09-22 15:18:55 +0300

    fixed prompt format typo

--- containers/ollama-env/ollama_train/train.py
+++ containers/ollama-env/ollama_train/train.py
@@ -551,8 +551,8 @@ def create_batch(task = "summary", model_1 = 0, model_2 = 1, start=0, end=None, 
                                               java = java,
                                               cs = cs,
                                               output = output,
-                                              example_summary: example_summary,
-                                              example_output: example_output)
+                                              example_summary = example_summary,
+                                              example_output = example_output)
             model = OllamaLLM(model=llm, num_predict=4096)
             prompt = ChatPromptTemplate.from_template(content_format)
             chain = prompt | model
